2959607e5e03914a5605fb74d02e269ff6fff57d
Ragina Akter <raginaakhter052@gmail.com>
2025-11-17 15:32:34 +0600

    demo for pusll

---
+++ src/components/ClinicDashboard/ClinicSettings/ClinicMessage/Demo.tsx
@@ -0,0 +1,10 @@
+
+const Demo = () => {
+    return (
+        <div>
+            <h1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto quisquam, aliquid ad est adipisci natus obcaecati placeat praesentium, repellat suscipit nihil doloremque dolore nisi necessitatibus sapiente, illum labore eum cum!</h1>
+        </div>
+    );
+};
+
+export default Demo;
