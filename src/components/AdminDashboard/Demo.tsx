1659474c780f70963cc625d1527cb9379047dbf8
RahulKhanSuvo <rahul.khan.suvo@gmail.com>
2025-12-04 11:55:55 +0600

    update here

---
+++ src/components/AdminDashboard/Demo.tsx
@@ -0,0 +1,11 @@
+const Demo = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem numquam
+      minus cumque? Dolorum praesentium eligendi est eius vitae eum quisquam
+      temporibus et error, adipisci atque nulla, commodi maiores, alias in.
+    </div>
+  );
+};
+
+export default Demo;
