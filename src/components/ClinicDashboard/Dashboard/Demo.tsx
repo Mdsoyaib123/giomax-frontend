a127e49a4ff42684c5bb213dafc29df63b0e1f5c
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-12-13 16:44:37 +0600

    update here

---
+++ src/components/ClinicDashboard/Dashboard/Demo.tsx
@@ -0,0 +1,12 @@
+const Demo = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit quibusdam,
+      explicabo maiores facere culpa iure quasi eligendi deleniti rem error,
+      excepturi esse molestias modi nobis architecto ipsam adipisci. Cumque,
+      neque?
+    </div>
+  );
+};
+
+export default Demo;
